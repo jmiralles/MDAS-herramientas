5073cff02386e6d34d70c237e0962a18ff1abc07
jmiralles <jmirallespaez@gmail.com>
2018-11-15 07:19:27 +0100

    added nginx and docker compose

--- docker-pipeline.sh
+++ docker-pipeline.sh
@@ -1,20 +1,7 @@
+#!/bin/bash
 set -e
 
-image='votingapp'
-registry=${REGISTRY:-'jmiralles99'}
-network='votingapp-network'
-
-docker network create $network || true
-
-# BUILD
-docker build -t $registry/$image -f ./src/votingapp/Dockerfile ./src/votingapp
-
-# INTEGRATION TESTS
-docker rm -f $image || true
-docker run --name $image -d --network $network $registry/$image
-
-docker build -t votingapp-test ./test
-docker run --rm --network $network -e VOTING_URL="http://$image:8080" votingapp-test
-
-# DELIVERY
-docker push $registry/$image
+docker-compose down
+docker-compose up -d --build
+docker-compose run --rm votingapp-test
+docker-compose push votingapp
